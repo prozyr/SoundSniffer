ec011bc69a7b2cce99e1aa64a752b725abd1c5f8
MavekGit <Wecki.Maciej@gmail.com>
2023-10-07 21:03:40 +0200

    Basic Chart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,12 @@ android {
 
 dependencies {
 
-    //implementation("com.gauravk.audiovisualizer:audiovisualizer:0.9.2")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
--- app/src/main/java/com/example/soundsniffier/MainActivity.java
+++ app/src/main/java/com/example/soundsniffier/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.soundsniffier;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LineChart chart = findViewById(R.id.chart);
+
+        int[] dataObjectsX = {1, 2, 3, 4, 5, 6};
+        int[] dataObjectsY = {11, 12, 13, 14, 15, 16};
+
+        List<Entry> entries = new ArrayList<>();
+
+        for (int i = 0; i < dataObjectsX.length; i++) {
+            // Tworzymy obiekty Entry dla danych X i Y
+            entries.add(new Entry(dataObjectsX[i], dataObjectsY[i]));
+        }
+
+        LineDataSet dataSet = new LineDataSet(entries, "Label");
+        // Możesz dalej dostosować wygląd dataSet, np. kolor, itp.
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate(); // Odświeżenie wykresu
     }
 }
