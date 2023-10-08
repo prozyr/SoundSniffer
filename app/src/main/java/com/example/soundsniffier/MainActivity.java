92fd5b8f5341aba0efb53c0a3a5e473e80488563
MavekGit <Wecki.Maciej@gmail.com>
2023-10-08 20:24:58 +0200

    Histogram trials

--- app/src/main/java/com/example/soundsniffier/MainActivity.java
+++ app/src/main/java/com/example/soundsniffier/MainActivity.java
@@ -4,9 +4,41 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.Menu;
 
+<<<<<<< Updated upstream
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+=======
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.util.Log;
+>>>>>>> Stashed changes
 
 import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
@@ -20,12 +52,30 @@ import com.example.soundsniffier.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+<<<<<<< Updated upstream
     private AppBarConfiguration mAppBarConfiguration;
+=======
+    private static final String TAG = "SoundSniffer";
+    private static final int PERMISSION_RECORD_AUDIO = 1;
+    private LineChart chart;
+
+    private BarChart barChart;
+    private AudioRecord audioRecord;
+    private HandlerThread audioThread;
+    private Handler audioHandler;
+    private List<Entry> entries = new ArrayList<>();
+
+    private List<Integer> entriesData = new ArrayList<>();
+
+    private List<BarEntry> barEntries = new ArrayList<>();
+    private int xValue = 0;
+>>>>>>> Stashed changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+<<<<<<< Updated upstream
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -33,6 +83,20 @@ public class MainActivity extends AppCompatActivity {
         if (binding.appBarMain.fab != null) {
             binding.appBarMain.fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show());
+=======
+        chart = findViewById(R.id.chart);
+        barChart = findViewById(R.id.barChart);
+
+        int sampleRate = 4000;  // TODO:
+        int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+        int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+        int bufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.RECORD_AUDIO}, PERMISSION_RECORD_AUDIO);
+        } else {
+            initializeAudioRecording(sampleRate, channelConfig, audioFormat, bufferSize);
+>>>>>>> Stashed changes
         }
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_content_main);
         assert navHostFragment != null;
@@ -48,6 +112,7 @@ public class MainActivity extends AppCompatActivity {
             NavigationUI.setupWithNavController(navigationView, navController);
         }
 
+<<<<<<< Updated upstream
         BottomNavigationView bottomNavigationView = binding.appBarMain.contentMain.bottomNavView;
         if (bottomNavigationView != null) {
             mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -55,6 +120,89 @@ public class MainActivity extends AppCompatActivity {
                     .build();
             NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
             NavigationUI.setupWithNavController(bottomNavigationView, navController);
+=======
+        audioThread = new HandlerThread("AudioThread");
+        audioThread.start();
+        audioHandler = new Handler(audioThread.getLooper());
+
+
+        audioHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                audioRecord.startRecording();
+                byte[] buffer = new byte[bufferSize];
+
+                while (!Thread.currentThread().isInterrupted()) {
+                    int bytesRead = audioRecord.read(buffer, 0, bufferSize);
+
+                    synchronized (lock) {
+                        for (int i = 0; i < bytesRead / 2; i++) {
+                            int audioSample = (int) ((buffer[i * 2] & 0xFF) | (buffer[i * 2 + 1] << 8));
+                            entries.add(new Entry(xValue++, audioSample));
+                            entriesData.add(new Integer(audioSample));          // Clean read data
+                        }
+
+                            Map<Integer, Integer> uniqueValues = new HashMap<>();
+                                // Counting unique values
+
+                            for(Integer histSample : entriesData)
+                            {
+                                if(histSample !=0) {
+                                    if (uniqueValues.containsKey(histSample)) {
+                                        uniqueValues.put(histSample, uniqueValues.get(histSample) + 1);
+                                    }
+                                    else {
+                                        uniqueValues.put(histSample, 1);
+                                    }
+                                }
+                            }
+
+                            for(Map.Entry<Integer,Integer> histogram: uniqueValues.entrySet()) {
+                                barEntries.add(new BarEntry(histogram.getKey()+1,histogram.getValue()));
+                            }
+
+
+
+                        if (entries.size() > bufferSize*4) { // TODO: LENGHT of dataset to draw in chart
+                            for (int i = 0; i < bytesRead; i++)
+                                entries.remove(0);
+                                entriesData.remove(0);
+                        }
+                    }
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            LineDataSet dataSet;
+                            BarDataSet barDataSet;
+                            synchronized (lock) {
+                                dataSet = new LineDataSet(new ArrayList<>(entries), "Data"); // Tworzymy kopię listy
+                                barDataSet = new BarDataSet(new ArrayList<>(barEntries),"LOL XD");
+                            }
+
+                            LineData lineData = new LineData(dataSet);
+                            chart.setData(lineData);
+                            chart.notifyDataSetChanged();
+                            chart.invalidate();
+                            BarData barData = new BarData(barDataSet);
+                            barChart.setData(barData);
+                            barChart.notifyDataSetChanged();
+                            barChart.invalidate();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (audioRecord != null) {
+            audioRecord.stop();
+            audioRecord.release();
+            audioThread.quitSafely();
+>>>>>>> Stashed changes
         }
     }
 
