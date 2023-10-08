0ed0c906a72e6672cc67e753cdb96e0c288218fb
prozyr <prozyr@gmail.com>
2023-10-08 17:38:29 +0200

    Live chart audio from microphone

--- app/src/main/java/com/example/soundsniffier/MainActivity.java
+++ app/src/main/java/com/example/soundsniffier/MainActivity.java
@@ -19,24 +19,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "SoundSniffer"; // Dodaj tag debugowania
+    private static final String TAG = "SoundSniffer";
+    private static final int PERMISSION_RECORD_AUDIO = 1;
+    private LineChart chart;
+    private AudioRecord audioRecord;
+    private HandlerThread audioThread;
+    private Handler audioHandler;
+    private List<Entry> entries = new ArrayList<>();
+
+    private int xValue = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        chart = findViewById(R.id.chart);
 
-
-        int sampleRate = 44100; // Standardowa częstotliwość próbkowania
-        int channelConfig = AudioFormat.CHANNEL_IN_MONO; // Jedno kanałowy
-        int audioFormat = AudioFormat.ENCODING_PCM_16BIT; // 16-bitowe dane PCM
-
+        int sampleRate = 8000;  // TODO: 
+        int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+        int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
         int bufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
 
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.RECORD_AUDIO}, PERMISSION_RECORD_AUDIO);
+        } else {
+            initializeAudioRecording(sampleRate, channelConfig, audioFormat, bufferSize);
+        }
+    }
+    private Object lock = new Object(); // Obiekt do synchronizacji
+    private void initializeAudioRecording(int sampleRate, int channelConfig, int audioFormat, int bufferSize) {
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -45,12 +81,9 @@ public class MainActivity extends AppCompatActivity {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            Log.e(TAG, "Brak uprawnień do nagrywania dźwięku.");
-
             return;
         }
-
-        AudioRecord audioRecord = new AudioRecord(
+        audioRecord = new AudioRecord(
                 MediaRecorder.AudioSource.MIC,
                 sampleRate,
                 channelConfig,
@@ -58,44 +91,72 @@ public class MainActivity extends AppCompatActivity {
                 bufferSize
         );
 
-        LineChart chart = findViewById(R.id.chart);
-
-        List<Entry> entries = new ArrayList<>();
-
-        audioRecord.startRecording();
-        byte[] buffer = new byte[bufferSize];
-        int xValue = 0;
+        audioThread = new HandlerThread("AudioThread");
+        audioThread.start();
+        audioHandler = new Handler(audioThread.getLooper());
 
 
-        long timeInterval = 5000, timeloop = 0, timeEvent = 0;
-        chart.invalidate();
+        audioHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                audioRecord.startRecording();
+                byte[] buffer = new byte[bufferSize];
 
-        while (true) { // przechwytywanieAktywne to zmienna kontrolująca przechwytywanie dźwięku
-            timeloop = System.currentTimeMillis();
+                while (!Thread.currentThread().isInterrupted()) {
+                    int bytesRead = audioRecord.read(buffer, 0, bufferSize);
 
+                    synchronized (lock) {
+                        for (int i = 0; i < bytesRead / 2; i++) {
+                            int audioSample = (int) ((buffer[i * 2] & 0xFF) | (buffer[i * 2 + 1] << 8));
+                            entries.add(new Entry(xValue++, audioSample));
+                        }
 
-            if( timeloop - timeEvent >= timeInterval) {
-                int bytesRead = audioRecord.read(buffer, 0, bufferSize);
+                        if (entries.size() > bufferSize*20) { // TODO: LENGHT of dataset to draw in chart
+                            for (int i = 0; i < bytesRead; i++)
+                                entries.remove(0);
+                        }
+                    }
 
-                for (int i = 0; i < bytesRead / 2; i++) {
-                    int audioSample = (int)((buffer[i * 2] & 0xFF) | (buffer[i * 2 + 1] << 8));
-                    // Przetwarzaj próbki audio i dodawaj je jako punkty na wykresie
-                    entries.add(new Entry(xValue++, audioSample));
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            LineDataSet dataSet;
+                            synchronized (lock) {
+                                dataSet = new LineDataSet(new ArrayList<>(entries), "Data"); // Tworzymy kopię listy
+                            }
+                            LineData lineData = new LineData(dataSet);
+                            chart.setData(lineData);
+                            chart.notifyDataSetChanged();
+                            chart.invalidate();
+                        }
+                    });
                 }
-
-                // Aktualizuj wykres na bieżąco
-
-                LineDataSet dataSet = new LineDataSet(entries, "Data");
-                LineData lineData = new LineData(dataSet);
-                Log.d(TAG, "Odczytano próbki audio: " + lineData);
-                chart.setData(lineData);
-                chart.notifyDataSetChanged();
-                chart.invalidate();
-                timeEvent = timeloop;
             }
+        });
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (audioRecord != null) {
+            audioRecord.stop();
+            audioRecord.release();
+            audioThread.quitSafely();
+        }
+    }
 
-            // Log.d(TAG, "Odczytano próbki audio: " + bytesRead);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_RECORD_AUDIO && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            int sampleRate = 44100;
+            int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+            int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+            int bufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+            initializeAudioRecording(sampleRate, channelConfig, audioFormat, bufferSize);
+        } else {
+            Log.e(TAG, "Brak uprawnień do nagrywania dźwięku.");
         }
     }
 }
+
