954e52f327b46ddbd0a17ad24d1a180ab3832d1d
prozyr <prozyr@gmail.com>
2023-10-08 16:28:22 +0200

    Draw chart from audio data microphone

--- app/src/main/java/com/example/soundsniffier/MainActivity.java
+++ app/src/main/java/com/example/soundsniffier/MainActivity.java
@@ -1,47 +1,101 @@
 package com.example.soundsniffier;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "SoundSniffer"; // Dodaj tag debugowania
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        int sampleRate = 44100; // Standardowa częstotliwość próbkowania
+        int channelConfig = AudioFormat.CHANNEL_IN_MONO; // Jedno kanałowy
+        int audioFormat = AudioFormat.ENCODING_PCM_16BIT; // 16-bitowe dane PCM
+
+        int bufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.e(TAG, "Brak uprawnień do nagrywania dźwięku.");
+
+            return;
+        }
+
+        AudioRecord audioRecord = new AudioRecord(
+                MediaRecorder.AudioSource.MIC,
+                sampleRate,
+                channelConfig,
+                audioFormat,
+                bufferSize
+        );
+
         LineChart chart = findViewById(R.id.chart);
-        int xmax = 0;
-        int ymax = 0;
-        int[] dataObjectsX = {1, 2, 3, 4, 5, 6};
-        int[] dataObjectsY = {11, 12, 13, 14, 15, 16};
 
         List<Entry> entries = new ArrayList<>();
 
+        audioRecord.startRecording();
+        byte[] buffer = new byte[bufferSize];
+        int xValue = 0;
 
-        for (int i = 0; i < dataObjectsX.length; i++) {
-            // Tworzymy obiekty Entry dla danych X i Y
-            entries.add(new Entry(dataObjectsX[i], dataObjectsY[i]));
+
+        long timeInterval = 5000, timeloop = 0, timeEvent = 0;
+        chart.invalidate();
+
+        while (true) { // przechwytywanieAktywne to zmienna kontrolująca przechwytywanie dźwięku
+            timeloop = System.currentTimeMillis();
+
+
+            if( timeloop - timeEvent >= timeInterval) {
+                int bytesRead = audioRecord.read(buffer, 0, bufferSize);
+
+                for (int i = 0; i < bytesRead / 2; i++) {
+                    int audioSample = (int)((buffer[i * 2] & 0xFF) | (buffer[i * 2 + 1] << 8));
+                    // Przetwarzaj próbki audio i dodawaj je jako punkty na wykresie
+                    entries.add(new Entry(xValue++, audioSample));
+                }
+
+                // Aktualizuj wykres na bieżąco
+
+                LineDataSet dataSet = new LineDataSet(entries, "Data");
+                LineData lineData = new LineData(dataSet);
+                Log.d(TAG, "Odczytano próbki audio: " + lineData);
+                chart.setData(lineData);
+                chart.notifyDataSetChanged();
+                chart.invalidate();
+                timeEvent = timeloop;
+            }
+
+
+            // Log.d(TAG, "Odczytano próbki audio: " + bytesRead);
         }
-
-        LineDataSet dataSet = new LineDataSet(entries, "Data");
-        // Możesz dalej dostosować wygląd dataSet, np. kolor, itp.
-        dataSet.setColors(Color.RED);
-        dataSet.setDrawCircleHole(true);
-        entries.add(new Entry(7,70));
-        LineDataSet dataSet1 = new LineDataSet(entries, "Data");
-
-
-        LineData lineData = new LineData(dataSet1);
-        chart.setData(lineData);
-        chart.invalidate(); // Odświeżenie wykresu
     }
 }
